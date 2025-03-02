aa432accfc7632e8869fb181bfff0e5d63c0f2ac
zahra-hojaty-moghaddam <zahrahojatymoghaddam@gmail.com>
2025-03-02 14:01:58 +0330

    Qestions part 1

---
+++ Qestions p1/questionP1.js
@@ -0,0 +1,46 @@
+//  Qestion1:)
+//  input:
+//    array1 = [ 1, 2, 'red'];
+//    array2 = [ 3, 4, 'orange'];
+//    array3 = [ 5, 6, 'pink'];
+//    array4 = [ 7, 8, 'blue'];
+
+//  output : 
+//    a:    array5 = [ 1, 2, 'red', 3, 4, 'orange', 5, 6, 'pink', 7, 8, 'blue']
+//    b:    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+   
+
+//  Qestion2:)
+//  input:
+//        fruits = ['apple' , 111 , 222 , 333 , 'orange' , 'grapes' , 'watermelon']
+//  output:
+//        fruits = ['apple' , 'the numbers is deleted' , 'orange' , 'grapes' , 'watermelon']
+
+
+//  Qestion3:)
+//  input:
+//        sentence = "That cat is very cute and smart. ";
+//  output:
+//        very qute
+
+
+//  Qestion4:)
+//  input:
+//  We have an object wizard with the following structure:
+//        firstName: "Harry", lastName: "potter", schoolName: "Hogwarts"
+//  Convert the product wizard into a pretty JSON string (with indentation) using JSON.stringify().
+//  Use an indentation of 4 spaces(or tab) to make the JSON more readable.
+//  output:
+//        {
+//            "firstName": "harry",
+//            "lastName": "potter",
+//            "schoolName": "Hogwarts"
+//        }
+
+
+//  Qestion5:)
+//  Create an object that contains information about employee
+//  Get and display all the keys of this object.
+//  Get and display all the values of this object.
+//  Add a new property called experience with the value 5 years to the object and then display the updated object.
+//  Display the employee object again.
